69ace53f78ddf284e6639be0436da9e3337f0aa0
proesch2 <roeschp@wit.edu>
2023-05-18 08:49:12 -0400

    add public mint for testing

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -13,7 +13,7 @@ contract NFT is ERC721{
 
     constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_){}
 
-    function mint(){
+    function mint() public {
         _mint(msg.sender, id++);
     }
 }
